4ad1dd428d47839a1c34c6910f0fe3369ff67bd6
zmoth <QianMoth@qq.com>
2024-09-05 09:42:27 +0800

    Add opencv to call the libdmtx decoder

---
+++ examples/opencv/main.cpp
@@ -0,0 +1,264 @@
+#include <assert.h>
+#include <dmtx.h>
+#include <math.h>
+#include <stdint.h>
+
+#include <opencv2/opencv.hpp>
+
+#define DMTX_DISPLAY_SQUARE 1
+#define DMTX_DISPLAY_POINT 2
+#define DMTX_DISPLAY_CIRCLE 3
+
+cv::Mat image, img, show, pointShow;
+DmtxImage *gImage = NULL;
+
+void buildMatrixRegion(DmtxRegion *reg)
+{
+    cv::Mat s = img.clone();
+    cv::rectangle(s, cv::Point(reg->boundMin.X, gImage->height - reg->boundMin.Y),
+                  cv::Point(reg->boundMax.X, gImage->height - reg->boundMax.Y), CV_RGB(0, 0, 255));
+
+    cv::line(s, cv::Point(reg->leftLine.locPos.X, gImage->height - reg->leftLine.locPos.Y),
+             cv::Point(reg->leftLine.locNeg.X, gImage->height - reg->leftLine.locNeg.Y), CV_RGB(255, 0, 0));
+    cv::line(s, cv::Point(reg->bottomLine.locPos.X, gImage->height - reg->bottomLine.locPos.Y),
+             cv::Point(reg->bottomLine.locNeg.X, gImage->height - reg->bottomLine.locNeg.Y), CV_RGB(0, 255, 0));
+
+    pointShow = img.clone();
+
+    cv::namedWindow("buildMatrixRegion", cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+    cv::imshow("buildMatrixRegion", s);
+    cv::waitKey();
+}
+
+void plotPoint(DmtxPixelLoc loc, int colorInt, int paneNbr, int dispType)
+{
+    cv::Scalar color;
+    DmtxVector2 point;
+    std::string winname;
+
+    point.X = loc.X;
+    point.Y = loc.Y;
+
+    switch (paneNbr) {
+        case 1:
+            winname = "plotPoint1";
+            break;
+        case 2:
+            winname = "plotPoint2";
+            break;
+        case 3:
+            winname = "plotPoint3";
+            break;
+        case 4:
+            winname = "plotPoint4";
+            break;
+        case 5:
+            winname = "plotPoint5";
+            break;
+        case 6:
+            winname = "plotPoint6";
+            break;
+        default:
+            winname = "showPlotPoint";
+    }
+
+    switch (colorInt) {
+        case 1:
+            color = CV_RGB(255, 0, 0);
+            break;
+        case 2:
+            color = CV_RGB(0, 255, 0);
+            break;
+        case 3:
+            color = CV_RGB(0, 0, 255);
+            break;
+        case 4:
+            color = CV_RGB(255, 255, 0);
+            break;
+        default:
+            color = CV_RGB(255, 255, 255);
+            break;
+    }
+
+    if (dispType == DMTX_DISPLAY_SQUARE) {
+        if (colorInt == 1) {
+            pointShow = img.clone();
+        }
+        cv::circle(pointShow, cv::Point((int)point.X, gImage->height - (int)point.Y), 3, color, 2);
+    } else if (dispType == DMTX_DISPLAY_POINT) {
+        int x = (int)(point.X + 0.5);
+        int y = (int)(gImage->height - point.Y + 0.5);
+        cv::circle(pointShow, cv::Point(x, y), 1, color, -1);
+    }
+
+    cv::namedWindow(winname, cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+    cv::imshow(winname, pointShow);
+    cv::waitKey(1);
+}
+
+// 计算交点
+cv::Point findIntersection(cv::Point p1, double theta1, cv::Point p2, double theta2)
+{
+    // 将角度转换为弧度
+    double rad1 = theta1 * M_PI / 180.0;
+    double rad2 = theta2 * M_PI / 180.0;
+
+    // 处理特殊情况：垂直直线
+    bool isVertical1 = std::abs(rad1) == M_PI / 2;
+    bool isVertical2 = std::abs(rad2) == M_PI / 2;
+
+    // 处理垂直直线的情况
+    if (isVertical1 && isVertical2) {
+        throw std::invalid_argument("Both lines are vertical, no unique intersection.");
+    } else if (isVertical1) {
+        // 第一条直线垂直，第二条直线非垂直
+        double x = p1.x;
+        double m2 = std::tan(rad2);
+        double b2 = p2.y - m2 * p2.x;
+        double y = m2 * x + b2;
+        return cv::Point((int)x, (int)y);
+    } else if (isVertical2) {
+        // 第二条直线垂直，第一条直线非垂直
+        double x = p2.x;
+        double m1 = std::tan(rad1);
+        double b1 = p1.y - m1 * p1.x;
+        double y = m1 * x + b1;
+        return cv::Point((int)x, (int)y);
+    }
+
+    // 计算斜率
+    double m1 = std::tan(rad1);
+    double m2 = std::tan(rad2);
+
+    // 如果斜率相同，则两条直线平行或重合，无法计算交点
+    if (m1 == m2) {
+        throw std::invalid_argument("Lines are parallel or coincident.");
+    }
+
+    // 求解直线方程中的常数项
+    double b1 = p1.y - m1 * p1.x;
+    double b2 = p2.y - m2 * p2.x;
+
+    // 求解 x
+    double x = (b2 - b1) / (m1 - m2);
+
+    // 求解 y
+    double y = m1 * x + b1;
+
+    return cv::Point((int)x, (int)y);
+}
+
+int main(int argc, char *argv[])
+{
+    dmtxCallbackBuildMatrixRegion(buildMatrixRegion);
+    dmtxCallbackPlotPoint(plotPoint);
+
+    DmtxDecode *dec;
+    DmtxRegion *reg;
+    DmtxMessage *msg;
+
+    // image = cv::imread("images/test_image01.bmp");
+    // image = cv::imread("images/test_image02.bmp");
+    // image = cv::imread("images/test_image03.bmp");
+    // image = cv::imread("images/test_image04.bmp");
+    // image = cv::imread("images/test_image05.bmp");
+    // image = cv::imread("images/test_image06.bmp");
+    // image = cv::imread("images/test_image07.bmp");
+    // image = cv::imread("images/test_image08.bmp");
+    // image = cv::imread("images/test_image09.bmp");
+    // image = cv::imread("images/test_image10.bmp");
+    // image = cv::imread("images/test_image11.bmp");
+    // image = cv::imread("images/test_image12.bmp");
+    // image = cv::imread("images/test_image13.bmp");
+    // image = cv::imread("images/test_image14.bmp");
+    // image = cv::imread("images/test_image15.bmp");
+    image = cv::imread("images/test_image16.bmp");
+    // image = cv::imread("images/test_image17.bmp");
+    // image = cv::imread("images/test_image18.bmp");
+
+    img = image.clone();
+    pointShow = img.clone();
+    show = img.clone();
+
+    int pack = DmtxPack32bppXRGB;
+    auto f = image.type();
+    if (f == CV_32FC3) {
+        pack = DmtxPack32bppXRGB;
+    } else if (f == CV_8UC3) {
+        pack = DmtxPack24bppRGB;
+    } else if (f == CV_8UC1) {
+        pack = DmtxPack8bppK;
+    } else {
+        return {};
+    }
+
+    gImage = dmtxImageCreate(image.data, image.cols, image.rows, pack);
+    assert(gImage != NULL);
+
+    /* Pixels from glReadPixels are Y-flipped according to libdmtx */
+    // dmtxImageSetProp(gImage, DmtxPropImageFlip, DmtxFlipY);
+
+    /* Start fresh scan */
+    dec = dmtxDecodeCreate(gImage, 1);
+    assert(dec != NULL);
+
+    dmtxDecodeSetProp(dec, DmtxPropSymbolSize, DmtxSymbolShapeAuto);
+    dmtxDecodeSetProp(dec, DmtxPropSquareDevn, 50);
+    dmtxDecodeSetProp(dec, DmtxPropEdgeThresh, 10);
+    // dmtxDecodeSetProp(dec, DmtxPropEdgeMin, 30);
+    // dmtxDecodeSetProp(dec, DmtxPropEdgeMax, 100);
+
+    for (;;) {
+        reg = dmtxRegionFindNext(dec, NULL);
+        if (reg == NULL) {
+            break;
+        }
+
+        msg = dmtxDecodeMatrixRegion(dec, reg, DmtxUndefined);
+        if (msg != NULL) {
+            std::cout << msg->outputIdx << " - " << msg->output << std::endl;
+
+            cv::circle(show, cv::Point(reg->leftLoc.X, gImage->height - reg->leftLoc.Y), 2, CV_RGB(255, 0, 0), -1);
+            cv::circle(show, cv::Point(reg->bottomLoc.X, gImage->height - reg->bottomLoc.Y), 2, CV_RGB(0, 255, 0), -1);
+
+            cv::circle(show, cv::Point(reg->topLoc.X, gImage->height - reg->topLoc.Y), 2, CV_RGB(255, 255, 0), -1);
+            cv::circle(show, cv::Point(reg->rightLoc.X, gImage->height - reg->rightLoc.Y), 2, CV_RGB(0, 255, 255), -1);
+
+            cv::Point bl, br, tl, tr;
+            bl = findIntersection(cv::Point(reg->leftLoc.X, gImage->height - reg->leftLoc.Y), 180 - reg->leftAngle,
+                                  cv::Point(reg->bottomLoc.X, gImage->height - reg->bottomLoc.Y),
+                                  180 - reg->bottomAngle);
+
+            br = findIntersection(cv::Point(reg->rightLoc.X, gImage->height - reg->rightLoc.Y), 180 - reg->rightAngle,
+                                  cv::Point(reg->bottomLoc.X, gImage->height - reg->bottomLoc.Y),
+                                  180 - reg->bottomAngle);
+
+            tl = findIntersection(cv::Point(reg->leftLoc.X, gImage->height - reg->leftLoc.Y), 180 - reg->leftAngle,
+                                  cv::Point(reg->topLoc.X, gImage->height - reg->topLoc.Y), 180 - reg->topAngle);
+
+            tr = findIntersection(cv::Point(reg->rightLoc.X, gImage->height - reg->rightLoc.Y), 180 - reg->rightAngle,
+                                  cv::Point(reg->topLoc.X, gImage->height - reg->topLoc.Y), 180 - reg->topAngle);
+
+            cv::line(show, bl, br, CV_RGB(0, 255, 0));
+            cv::line(show, br, tr, CV_RGB(0, 255, 255));
+            cv::line(show, tr, tl, CV_RGB(0, 255, 255));
+            cv::line(show, tl, bl, CV_RGB(255, 0, 0));
+
+            dmtxMessageDestroy(&msg);
+
+            break;
+        }
+        dmtxRegionDestroy(&reg);
+    }
+
+    dmtxDecodeDestroy(&dec);
+    dmtxImageDestroy(&gImage);
+
+    // cv::namedWindow("pointShow", cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+    // cv::imshow("pointShow", pointShow);
+    // cv::waitKey();
+
+    cv::namedWindow("show", cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+    cv::imshow("show", show);
+    cv::waitKey();
+}
